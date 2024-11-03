1ff940835523228b302c0f657237b4c41f633433
Diana Makarova <dishk@auto.yandex.ru>
2024-11-03 20:41:12 +0500

    first commit

---
+++ src/main/java/PageObject/OrderPages.java
@@ -0,0 +1,14 @@
+package PageObject;
+
+import org.openqa.selenium.WebDriver;
+
+public class OrderPages {
+    WebDriver driver;
+
+    public OrderPages(WebDriver driver) {
+        this.driver = driver;
+    }
+
+
+
+}
